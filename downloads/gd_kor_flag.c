d9620d0b391542508bd5dd4efd920a91aec93668
41223208 <144991380+41223208@users.noreply.github.com>
2023-12-14 06:46:30 +0000

    大致作業10個練習繪製圖形

--- downloads/gd_kor_flag.c
+++ downloads/gd_kor_flag.c
@@ -1,36 +1,47 @@
 #include <stdio.h>
-
-void draw_korean_flag() {
-    FILE *file = fopen("korean_flag.pbm", "w");
-
-    if (file == NULL) {
-        fprintf(stderr, "Unable to create the file.\n");
-        return;
-    }
-
-    // PBM header
-    fprintf(file, "P1\n");
-    fprintf(file, "600 400\n");  // Width and height
-    fprintf(file, "# Korean Flag\n");
-
-    // Drawing the flag
-    for (int i = 0; i < 400; i++) {
-        for (int j = 0; j < 600; j++) {
-            // Define the colors based on the flag pattern
-            int isBlue = (i >= 200) ? 1 : 0;
-            int isWhite = (i >= 200 && i <= 300 && j >= 200 && j <= 400) ? 1 : 0;
-            int isRed = (i >= 200 && j <= 200) ? 1 : 0;
-
-            // Output the color
-            fprintf(file, "%d ", (isBlue || isWhite || isRed) ? 1 : 0);
-        }
-        fprintf(file, "\n");
-    }
-
-    fclose(file);
-}
-
+#include <gd.h>
+#include <math.h>
 int main() {
-    draw_korean_flag();
+    int width = 600, height = 400;
+    gdImagePtr img = gdImageCreateTrueColor(width, height);
+    int white = gdImageColorAllocate(img, 255, 255, 255);
+    int black = gdImageColorAllocate(img, 0, 0, 0);
+    int red = gdImageColorAllocate(img, 255, 0, 0);
+    int blue = gdImageColorAllocate(img, 0, 0, 255);
+
+    // Draw white background
+    gdImageFilledRectangle(img, 0, 0, width, height, white);
+
+    // Draw blue rectangle
+    gdImageFilledRectangle(img, 0, 0, width, height / 2, blue);
+
+    // Draw red circle
+    int cx = width / 2, cy = height / 4;
+    int r = height / 5;
+    gdImageFilledArc(img, cx, cy, r * 2, r * 2, 0, 360, red, gdArc);
+
+    // Draw black bars
+    int bar_width = width / 10, bar_height = height / 10;
+    int x = 0, y = 0;
+    for (int i = 0; i < 4; i++) {
+        x = width / 2 - bar_width / 2;
+        y = i * bar_height + height / 4;
+        gdImageFilledRectangle(img, x, y, x + bar_width, y + bar_height, black);
+    }
+    for (int i = 0; i < 4; i++) {
+        x = i * bar_width + width / 4;
+        y = height / 2 - bar_height / 2;
+        gdImageFilledRectangle(img, x, y, x + bar_width, y + bar_height, black);
+    }
+
+    // Save image to file
+    FILE *fp;
+    fp = fopen("korean_flag.png", "wb");
+    gdImagePng(img, fp);
+    fclose(fp);
+
+    // Free memory
+    gdImageDestroy(img);
+
     return 0;
 }
